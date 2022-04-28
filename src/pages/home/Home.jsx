5222644e319dfec002a248457ab2d52a30317db2
Henrique Ohnesorge Costa <51917267827@info.scs>
2022-04-28 09:47:34 -0300

    initial home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,63 @@
+import fab_logo from "../../assets/fab-logo.png";
+import logo from "../../assets/logo.png";
+import { React, useState, useEffect } from 'react';
+
+import { mock } from "../../services/api";
+
 export default function Home() {
-    return(
-        <div>
-            <p>Poderosíssima home</p>
-        </div>
+
+    const [list, setList] = useState([]);
+    const [search, setSearch] = useState('');
+
+    function getObjects() {
+        mock.get('/object')
+            .then(resposta => {
+                if (resposta.status === 200) {
+                    setList(resposta.data)
+                };
+            })
+            .catch(erro => console.log(erro));
+    }
+
+    useEffect(getObjects, []);
+
+    function toList() {
+
+        var r = new RegExp(search.toLowerCase(), "g")
+
+        return (
+            list.map(o =>
+                r.test(o.name.toLowerCase()) ?
+                    <div class="object">{o.name}</div>
+                    : null
+            )
+        )
+    }
+
+    function onChange(event) {
+        setSearch(event.target.value)
+    }
+
+    return (
+        <main className="cam_page_modal">
+            <header>
+                <img src={fab_logo} alt="" />
+                <img src={logo} alt="" />
+            </header>
+            <div className="cam_content">
+                <section className="search">
+                    <input type="text" placeholder="O que você procura...?" onChange={onChange} />
+                    <div className="list">
+                        {
+                            toList()
+                        }
+                    </div>
+                </section>
+
+                <section className="camera">
+
+                </section>
+            </div>
+        </main >
     )
 }
--- src/services/api.js
+++ src/services/api.js
@@ -4,6 +4,10 @@ export const db = axios.create({
     baseURL: 'http://localhost:5000/api'
 })
 
+export const mock = axios.create({
+    baseURL: 'https://626a808e737b438c1c496638.mockapi.io'
+})
+
 export const cam = axios.create({
     baseURL: 'https://api.meraki.com/api/v1'
 })
