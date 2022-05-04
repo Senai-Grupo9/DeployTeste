fec54beba864767115de0bf241fecc6af4640011
Rafael Rocha Tavares <51900217856@info.scs>
2022-05-04 09:31:33 -0300

    remoção npm build

--- src/pages/logusuario/LogUsuario.jsx
+++ src/pages/logusuario/LogUsuario.jsx
@@ -1,63 +1,34 @@
 import fab_logo from "../../assets/fab-logo.png";
 import logo from "../../assets/logo.png";
+import adicionar from "../../assets/adicionar.png";
 import { React, useState, useEffect } from 'react';
 
 import { mock } from "../../services/api";
 
 export default function LogUsuario() {
 
-    const [list, setList] = useState([]);
-    const [search, setSearch] = useState('');
-
-    function getObjects() {
-        mock.get('/object')
-            .then(resposta => {
-                if (resposta.status === 200) {
-                    setList(resposta.data)
-                };
-            })
-            .catch(erro => console.log(erro));
-    }
-
-    useEffect(getObjects, []);
-
-    function toList() {
-
-        var r = new RegExp(search.toLowerCase(), "g")
-
-        return (
-            list.map(o =>
-                r.test(o.name.toLowerCase()) ?
-                    <div class="object">{o.name}</div>
-                    : null
-            )
-        )
-    }
-
-    function onChange(event) {
-        setSearch(event.target.value)
-    }
-
     return (
         <main className="cam_page_modal">
             <header>
-                <img src={fab_logo} alt="" />
-                <img src={logo} alt="" />
+                <div className="container_nav">
+                    <nav className="content_logo">
+                        <img src={fab_logo} alt="Logo da FabSoluções" />
+                        <img src={logo} alt="Logo do Sistema Severino" />
+                    </nav>
+                    <nav className="content_pages">
+                        <a href="" className="usuario_nav">Usuários</a>
+                        <a href="" className="registros_nav">Registros</a>
+                        <a href="" className="home_nav">Home</a>
+                    </nav>
+                </div>
             </header>
-            <div className="cam_content">
-                <section className="search">
-                    <input type="text" placeholder="O que você procura...?" onChange={onChange} />
-                    <div className="list">
-                        {
-                            toList()
-                        }
-                    </div>
-                </section>
-
-                <section className="camera">
-
-                </section>
-            </div>
+            <article className="cam_content">
+                <span>
+                    <img src={adicionar} href="#" alt="Botão de adicionar" />
+                    <a href="#">Cadastrar Novo Usuário</a>
+                </span>
+                <table></table>
+            </article>
         </main >
     )
 }
