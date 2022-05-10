f7ec5d15a664d9f7fa99dde6a322983c990e43d7
Rafael Rocha Tavares <51900217856@info.scs>
2022-05-10 10:25:54 -0300

    string do localhost

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const db = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://backfab.azurewebsites.net/api'
 })
 
 export const mock = axios.create({
