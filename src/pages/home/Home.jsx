c8668385792be963484e63481df5e452e431ad7e
Henrique Ohnesorge Costa <51917267827@info.scs>
2022-05-31 08:24:51 -0300

    maybe

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -78,8 +78,24 @@ export default function Home() {
     return (
         <main className="cam_page_modal">
             <header>
-                <img src={fab_logo} alt="" />
-                <img src={logo} alt="" />
+                <div className="container_nav">
+                    <nav className="content_logo">
+                        <div className="popovercontainer">
+                            <img src={fab_logo} alt="logo da fab soluções" className="" />
+                            <span className="popover">
+                                FabSoluções<br />
+                                ID 509103<br />
+                                CheckIN 8:15<br />
+                            </span>
+                        </div>
+                        <img src={logo} alt="Logo do Sistema Severino" />
+                    </nav>
+                    <nav className="content_pages">
+                        <a href="/log" className="">Usuários</a>
+                        <a href="/checkin" className="">Registros</a>
+                        <a href="/home" className="marcado_home">Home</a>
+                    </nav>
+                </div>
             </header>
             <div className="cam_content">
                 <section className="search">
